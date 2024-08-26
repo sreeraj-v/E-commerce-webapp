630b2b66d9d6dada168cfdbf2fe7b4804eb7e5f6
Sreeraj <srj2255000@gmail.com>
2024-08-26 23:59:26 +0530

    fixed errors

--- helpers/address.js
+++ helpers/address.js
@@ -33,8 +33,23 @@ const addressHelper = {
     createAddress : async(userId,addressData)=>{
         try{
             const newAddress = new Address({
-                us
+                userId,
+                ...addressData
             })
+            await newAddress.save()
+            return newAddress;
+        }catch(error){
+            console.error("Error creating address: ", error);
+            throw error;            
         }
+    },
+
+    getUserAddresses : async(userId)=>{
+        try{
+            return await Address.find({userId}).lean()
+        } catch (error) {
+                        console.error("Error fetching addresses: ", error);
+                        throw error;
+                    }
     }
 }
