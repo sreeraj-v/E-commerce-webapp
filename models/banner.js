9f0c77084aabd8219b2e0675e5cb7ad76cb22ebf
Sreeraj <srj2255000@gmail.com>
2024-09-30 00:02:00 +0530

    fixed some errors

--- models/banner.js
+++ models/banner.js
@@ -1,19 +1,18 @@
 const mongoose = require('mongoose');
 
-const mainBanner =  mongoose.Schema(
+const mainBannerSchema = mongoose.Schema(
   {
-  title: { type: String, required: true },
-  subtitle: { type: String, required: true },
-  description: { type: String, required: true },
-  image: { type: String, required: true },
-  link: { type: String, required: true },
-  displayOrder: { type: Number, required: true }
-}, 
-{ timestamps: true }
+    title: { type: String, required: true },
+    subtitle: { type: String, required: true },
+    description: { type: String, required: true },
+    image: { type: String, required: true },
+    link: { type: String, required: true },
+    displayOrder: { type: Number, required: true },
+  },
+  { timestamps: true }
 );
 
+const MainBanner = mongoose.model('MainBanner', mainBannerSchema);
 
+module.exports = MainBanner;
 
-const MainBanner = mongoose.model('Mainbanner', mainBanner);
-
-module.exports = {MainBanner};
