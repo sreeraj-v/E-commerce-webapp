e308d5e623dd261c9f68c4df607d22f97da9ca11
Sreeraj <srj2255000@gmail.com>
2025-11-21 22:49:47 +0530

    fixed email registration issue in render

--- config/nodemailer/emailer.js
+++ config/nodemailer/emailer.js
@@ -3,7 +3,9 @@ const nodemailer = require("nodemailer")
 const {EMAIL_USER,EMAIL_PASS} = process.env
 
 const transporter = nodemailer.createTransport({ 
-  service:"Gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth:{
     user:EMAIL_USER,
     pass:EMAIL_PASS
